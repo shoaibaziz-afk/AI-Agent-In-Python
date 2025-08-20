3e9d6e2a0deae70a00f4189fac7fd58eeac1028b
shoaibaziz <shoaib.aziz.space@gmail.com>
2025-08-21 01:58:08 +0300

    Added Function Calling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 from call_functions import available_functions
 from prompts import system_prompt
+from call_functions import call_function, available_functions
 
 
 def main():
@@ -51,9 +52,20 @@ def generate_content(client, messages, verbose):
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception ("empty function all result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 if __name__ == "__main__":
     main()
